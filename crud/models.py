9a9870f49321dbd2a0198ea7fd4b0adedec284fc
Alexandr Zubakin <zubakin.alexandr98@gmail.com>
2022-04-21 14:50:54 +0700

    remove changes

--- crud/models.py
+++ crud/models.py
@@ -1,13 +1,27 @@
+from turtle import mode
 from django.db import models
+import uuid
+
+from django.forms import BooleanField
 
 # Create your models here.
 
-class CategoriesProducts(models.Model):
-    pass
+class CategoriesOfProducts(models.Model):
+    name = models.CharField(max_length=100)
+    seq = models.IntegerField(unique=True)
+
 
 class GroupsProducts:
-    pass
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    category_id = models.ForeignKey('CategoriesOfProducts.id')
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000)
+    seq = models.IntegerField(unique=True)
 
+    
 class Products:
-    pass
+    group_id = models.ForeignKey('GroupsOfProduct.id', on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    price = models.FloatField()
+    hidden = BooleanField()
 
--- crud/urls.py
+++ crud/urls.py
@@ -1,8 +1,2 @@
 from django.urls import path
 
-urlpattens = [
-    path('/product-categories/'),
-    path('/product-groups/'),
-    path('/product-groups/<id:uuid>'),
-    path('/products/')
-]
