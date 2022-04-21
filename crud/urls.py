26a72babbb06c440bfff6db19be98ee865d129ec
Alexandr Zubakin <zubakin.alexandr98@gmail.com>
2022-04-21 19:44:43 +0700

    add get, post, put, delete methods for categories

--- crud/urls.py
+++ crud/urls.py
@@ -1,2 +1,8 @@
 from django.urls import path
+from .views import *
 
+
+urlpatterns = [
+    path('product-categories', CategoriesAPIView.as_view()),
+    path('product-categories/<int:pk>', CategoriesAPIView.as_view())
+]
--- crud/views.py
+++ crud/views.py
@@ -1,3 +1,58 @@
+from django.forms import model_to_dict
 from django.shortcuts import render
-
+from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import CategoriesOfProducts
+from .serializers import CategoriesSerializer
 # Create your views here.
+
+
+# class CategoriesAPIView(generics.ListAPIView):
+#     queryset = CategoriesOfProducts.objects.all()
+#     serializer_class = CategoriesSerializer
+
+
+class CategoriesAPIView(APIView):
+
+    def get(self, request):
+        lst = CategoriesOfProducts.objects.all()
+        return Response({'categories': CategoriesSerializer(lst, many=True).data})
+
+    def post(self, request):
+        serializer = CategoriesSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        
+        return Response({'category': serializer.data})
+
+    def put(self, request, *args, **kwargs):
+        pk = kwargs.get('pk', None)
+        if not pk:
+            return Response({'error': "Method PUT not allowed"})
+
+        try:
+            instance = CategoriesOfProducts.objects.get(pk=pk)
+        except:
+            return Response({'error': "Object does not exists"})
+
+        serializer = CategoriesSerializer(data=request.data, instance=instance)
+        serializer.is_valid()
+        serializer.save()
+        return Response({'category': serializer.data})
+
+    def delete(self, request, *args, **kwargs):
+        pk = kwargs.get('pk')
+        if not pk:
+            return Response({"error": "Method delete is not allowed"})
+
+        try:
+            obj = CategoriesOfProducts.objects.get(pk=pk)
+            obj.delete()
+        except:
+            return Response({"error": "Such object does not exits"})
+
+        return Response({"status": 'success', 'message': f"Object with {str(pk)} has been deleted"})
+
+        
+
